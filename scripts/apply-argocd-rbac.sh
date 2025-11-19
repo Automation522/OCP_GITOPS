6226c0bf2ef6156551cb93542e86f2ddba7b67b6
skyr <skyriacopoulos@fr.scc.com>
2025-11-19 11:31:44 +0100

    correction de la commande oc auth

--- scripts/apply-argocd-rbac.sh
+++ scripts/apply-argocd-rbac.sh
@@ -17,10 +17,22 @@ echo "Applying Argo CD RBAC manifests in namespace ${ARGOCD_NAMESPACE}..."
 oc apply -f "${role_file}"
 oc apply -f "${rolebinding_file}"
 
-echo "Verifying permissions for user ${ARGOCD_USER}..."
 oc auth can-i --as="${ARGOCD_USER}" -n "${ARGOCD_NAMESPACE}" --resource=applications.argoproj.io get
-oc auth can-i --as="${ARGOCD_USER}" -n "${ARGOCD_NAMESPACE}" --resource=applications.argoproj.io create
-oc auth can-i --as="${ARGOCD_USER}" -n "${ARGOCD_NAMESPACE}" --resource=appprojects.argoproj.io get
-oc auth can-i --as="${ARGOCD_USER}" -n "${ARGOCD_NAMESPACE}" --resource=appprojects.argoproj.io create
+check_permission() {
+  local verb=$1
+  local resource=$2
+  local api_group=$3
+  if oc auth can-i --as="${ARGOCD_USER}" -n "${ARGOCD_NAMESPACE}" "${verb}" "${resource}".${api_group} >/dev/null 2>&1; then
+    echo "✔ ${ARGOCD_USER} can ${verb} ${resource}.${api_group}"
+  else
+    echo "✖ ${ARGOCD_USER} cannot ${verb} ${resource}.${api_group}" >&2
+  fi
+}
+
+echo "Verifying permissions for user ${ARGOCD_USER}..."
+check_permission get applications argoproj.io
+check_permission create applications argoproj.io
+check_permission get appprojects argoproj.io
+check_permission create appprojects argoproj.io
 
 echo "Argo CD RBAC applied. User ${ARGOCD_USER} should now be able to manage Argo CD resources."
